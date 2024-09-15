fd4e268a900d9f171354dfc58bb27da6bdbf6611
YuvalC <>
2024-09-16 00:07:23 +0300

    Added 32bit print proc.

--- bootloader/stage_1/utilities.asm
+++ bootloader/stage_1/utilities.asm
@@ -1,9 +1,16 @@
-[bits 16]
+video_mem_start equ 0xb8000
 
+[bits 16]
+clear_screen:
+    mov ah, 0x00        ; Set video mode function
+    mov al, 0x03        ; Mode 3: 80x25 text mode
+    int 0x10            ; BIOS interrupt to set mode
+    ret                 ; Return to caller
 
 ; Prints a string to the screen
 ; Params:
 ;   - ds:si points to string
+[bits 16]
 print16_loop:
     ; save registers we will modify
     push ax
@@ -20,3 +27,28 @@ print16_loop:
     .done:
         pop ax  
         ret
+
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+[bits 32]
+print32_loop:
+    pusha
+
+    mov ebx, video_mem_start  ; Set EBX to point to the video memory base address
+    mov ecx, 0x07            ; Attribute byte (light gray on black)
+
+    .loop:
+        lodsb
+        cmp al, 0
+        je .done
+
+        mov [ebx], al
+        mov [ebx+1], cl
+        add ebx, 2
+
+        jmp .loop
+
+    .done:
+        popa
+        ret                      ; Return from procedure

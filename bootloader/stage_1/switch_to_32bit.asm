b9d4c81289111c88465c884c9a2f07733fd90043
YuvalC <>
2024-09-25 21:28:53 +0300

    Added enable_a20 proc.

--- bootloader/stage_1/switch_to_32bit.asm
+++ bootloader/stage_1/switch_to_32bit.asm
@@ -1,5 +1,13 @@
 [bits 16]
+enable_A20:
+    in al, 0x92              ; Read the value from port 0x92
+    or al, 0x02              ; Set bit 1 (A20 enable)
+    out 0x92, al             ; Write the modified value back to port 0x92
+    ret                      ; A20 is now enabled
+
+[bits 16]
 switch_to_32bit:
+    call enable_A20
     cli                     ; 1. disable interrupts
     lgdt [gdt_descriptor]   ; 2. load GDT descriptor
     mov eax, cr0
@@ -7,7 +15,7 @@ switch_to_32bit:
     mov cr0, eax
     jmp CODE_SEG:init_32bit ; 4. far jump
     ret
-    
+
 [bits 32]
 init_32bit:
     mov ax, DATA_SEG        ; 5. update segment registers

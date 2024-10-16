8921da8f6de4b0fd505acdd91bf17f18a27239aa
YuvalC <>
2024-10-16 16:24:25 +0300

    Wip

--- bootloader/stage_2/loader.asm
+++ bootloader/stage_2/loader.asm
@@ -8,7 +8,7 @@ NUM_SECTORS equ 2
 KERNEL_SECTORS equ 1
 
 ; where to load the kernel to
-KERNEL_OFFSET equ 0x1000
+KERNEL_OFFSET equ 0x500
 
 ; BIOS sets boot drive in 'dl'; store for later use
 mov [BOOT_DRIVE], dl
@@ -25,14 +25,21 @@ start:
     mov es, ax
     
     print16 stage_2_start
-
+    
     call enable_A20
     call switch_to_unreal_mode
 
+    ; 
 
 [bits 16]
-unreal_entry:
-    ; load kernel from disk
+load_disk:
+    push ds
+    push es
+    
+    mov ax, 0x0000    ; Set data segment to 0x0000
+    mov ds, ax
+    mov es, ax
+
     mov bx, KERNEL_OFFSET         ; bx -> destination address
     mov cl, (1 + NUM_SECTORS) + 1 ; start from sector 4 (as sector our bootloader is 3 sectors long)
     mov dh, KERNEL_SECTORS        ; dh -> num sectors (1 sector)
@@ -40,15 +47,31 @@ unreal_entry:
     call load_from_disk
     print16 disk_loaded_log
 
-    call switch_to_32bit
-    call update_32bit_stack
+    pop es
+    pop ds
 
+    ret
 
-[bits 32]
-start_32:
-    ; print32 debug_log
-    call 0x1100
+[bits 16]
+unreal_entry:
+    call load_disk
+    
+
+    mov bx, [my_word]       ; Load the WORD into AX (0xABCD)
+    mov eax, 0x100000
+    mov word [ds:eax], bx
+
     stop
+
+
+;     call switch_to_32bit
+;     call update_32bit_stack
+
+
+; [bits 32]
+; start_32:
+;     stop
+;     call 0x1100
 
 
 %include "common/logs.asm"
@@ -59,6 +82,8 @@ start_32:
 
 ; boot drive variable
 BOOT_DRIVE db 0
+
+my_word dw 0xABCD       ; Define a WORD with the value 0xABCD
 
 ; padding the program to one sector
 TOTAL_SIZE equ 512 * NUM_SECTORS  

d118e1a1e07cd34ebe65aec69b8bbe141a62bf6d
Yuval567 <YuvalCohen20493@gmail.com>
2025-09-02 21:32:42 +0300

    Refactor constants in bootloader for clarity and consistency

--- bootloader/common/constants.asm
+++ bootloader/common/constants.asm
@@ -1,6 +1,11 @@
 ; Stage 1 bootloader constants
 LOADER_NUM_SECTORS equ 2    ; Number of sectors in the second stage bootloader
 LOADER_START_SECTOR equ 2   ; sector 1 is the MBR, so we start from sector 2
-LOADER_ADDR equ 0x7e00      ; address in memory where the second stage bootloader will be loaded 
+LOADER_ADDRESS equ 0x7e00      ; address in memory where the second stage bootloader will be loaded 
 
 
+; Stage 2 bootloader constants
+SECTOR_SIZE_BYTES equ 512           ; Size of a sector in bytes
+BUFFER_ADDRESS equ 0x500            ; Address in memory where sectors will be loaded temporarily
+KERNEL_NUM_SECTORS equ 15           ; Number of sectors in the kernel image
+KERNEL_START_ADDRESS equ 0x100400   ; Address where the kernel will be loaded in memory

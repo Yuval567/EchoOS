0e4c73b94a44980ff5b42ddaa52478001aacad46
Yuval567 <YuvalCohen20493@gmail.com>
2025-09-03 00:15:29 +0300

    Split responsibilities to separated files .

---
+++ bootloader/stage_2/protected_mode.asm
@@ -0,0 +1,48 @@
+; Enables the A20 address line, allowing access to memory above 1MB.
+; Enables the A20 line via port 0x92 (System Control Port A)
+; Parameters: None
+; Returns: None
+[bits 16]
+enable_A20_gate:
+    push ax
+
+    in al, 0x92     ; Read the value from port 0x92
+    or al, 0x02     ; Set bit 1 (A20 enable)
+    out 0x92, al    ; Write the modified value back to port 0x92
+
+    pop ax
+    ret  
+
+; Loads the GDT descriptor into the GDTR register.
+; Parameters: gdt_descriptor - pointer to the GDT descriptor
+; Returns: None
+[bits 16]
+load_gdt_descriptor:
+    lgdt [gdt_descriptor]
+    ret
+
+; Enables protected mode by setting the PE bit in the CR0 register.
+; Cancels interrupts to ensure a safe transition.
+; Parameters: None
+; Returns: None
+[bits 16]
+enable_protected_mode:
+    push eax
+    
+    cli
+    mov eax, cr0    ; read CR0 register
+    or eax, 0x1     ; set PE bit (bit 0) in CR0
+    mov cr0, eax    ; write back to CR0
+    
+    pop eax
+    ret
+
+; Switches the CPU to protected mode and jumps to the protected_mode_entry label.
+; This function assumes that the GDT has already been loaded and that the
+; segment selectors are set up correctly.
+; Parameters: None
+; Returns: None
+[bits 16]
+switch_to_protected_mode:
+    call enable_protected_mode
+    jmp CODE_SEG:protected_mode_entry   ; far jump to clear prefetch queue and switch to protected mode
---
+++ bootloader/stage_2/unreal_mode.asm
@@ -0,0 +1,27 @@
+; Transitions the CPU from real mode to "unreal mode," allowing access to all memory (up to 4GB) 
+; while remaining in real mode by temporarily enabling protected mode and loading 32-bit segment limits.
+; Parameters: None
+; Calls: enable_protected_mode
+[bits 16]
+switch_to_unreal_mode:
+    push ds                     ; save DS   
+
+    call enable_protected_mode  ; enable protected mode
+
+    jmp $+3                     ; clear the instruction pre-fetch queue
+
+    ; update segment registers to use the data segment selector in the GDT
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    ; now disable protected mode, but keep the segment registers
+    and al,0xFE                 ; clear PE bit in AL
+    mov  cr0, eax               ; disable protected mode, back to real mode
+    sti                         ; re-enable interrupts
+    
+    pop ds                      ; restore DS
+    
+    call unreal_mode_entry
